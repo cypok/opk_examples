3ca01011c8732dc21357fbaf089b27d860f02882
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2015-02-18 20:02:13 +0600

    Simple example for conditions

---
+++ feminist.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    
+    printf("I will tell you your destination.\n");
+    printf("Are you a woman (y/n)?\n");
+
+    char woman_answer;
+    scanf("%c", &woman_answer);
+
+    if (woman_answer == 'y') {
+        printf("Hey! You should go to McDonalds!\n");
+    } else {
+        if (woman_answer == 'n') {
+            printf("Yeah... You will be a cool programmer!\n");
+        }
+        //else {
+        //    printf("You are not a human, go away.\n");
+        //}
+    }
+    return 0;
+}

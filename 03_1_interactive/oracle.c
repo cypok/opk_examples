15244904b9950f98591f6f07a3323df67819d7ca
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2015-03-04 23:09:35 +0600

    Simplier conditions sample

---
+++ 03_1_interactive/oracle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("I will tell you your destination.\n");
+    printf("Are you a woman (y/n)?\n");
+
+    char woman_answer;
+    scanf("%c", &woman_answer);
+
+    if (woman_answer == 'y') {
+        printf("Hey! You should not be a programmer.\n");
+    } else {
+        if (woman_answer == 'n') {
+            printf("Yeah... You will be a cool programmer!\n");
+        }
+        else {
+            printf("You are not a human, go away.\n");
+        }
+    }
+    return 0;
+}
